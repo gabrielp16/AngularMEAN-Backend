a371a30606248a6d11747d9e5f8a10274eb68462
Gabriel Peña <gabriel.p16@gmail.com>
2020-06-23 18:58:40 -0500

    Actualizacion de imagenes

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
+// Habilitar CORS - Access-Control-Allow-Origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
+
+
 
 //Body Parser
 // parse application/x-www-form-urlencoded
